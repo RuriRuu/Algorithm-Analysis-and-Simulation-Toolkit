301724bb0acb4b08c826a0e390642b12153c7873
AbigailSallegue <202301384@iacademy.edu.ph>
2026-04-01 22:16:17 +0800

    Add files via upload

---
+++ RecursiveFunctionSimulation_PT2/src/Recursion__Module/Functions/Factorial_Module.java
@@ -0,0 +1,16 @@
+package Recursion__Module.Functions;
+
+public class Factorial_Module {
+    public static int factorial(int n) {
+        System.out.println("factorial(" + n + ")");
+        if (n == 1) {
+            System.out.println(" -> 1 (base case)");
+            return 1;
+        } else {
+            System.out.println(" -> " + n + " * factorial(" + (n - 1) + ")");
+            int result = n * factorial(n - 1);
+            System.out.println("Return factorial(" + n + ") = " + result);
+            return result;
+        }
+    }
+}
